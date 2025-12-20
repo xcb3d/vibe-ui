41291f3c4e6f432595a9ea862681aea417d2d83f
xcb3d <xcode3d@gmail.com>
2025-12-20 16:43:44 +0700

    feat: add collapsible theme overrides with data-slot attributes

--- packages/registry/ui/collapsible.tsx
+++ packages/registry/ui/collapsible.tsx
@@ -2,11 +2,45 @@
 
 import * as React from "react";
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
+import { cn } from "@/lib/utils";
 
-const Collapsible = CollapsiblePrimitive.Root;
+const Collapsible = React.forwardRef<
+  React.ComponentRef<typeof CollapsiblePrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <CollapsiblePrimitive.Root
+    ref={ref}
+    data-slot="collapsible"
+    className={cn(className)}
+    {...props}
+  />
+));
+Collapsible.displayName = "Collapsible";
 
-const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
+const CollapsibleTrigger = React.forwardRef<
+  React.ComponentRef<typeof CollapsiblePrimitive.CollapsibleTrigger>,
+  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.CollapsibleTrigger>
+>(({ className, ...props }, ref) => (
+  <CollapsiblePrimitive.CollapsibleTrigger
+    ref={ref}
+    data-slot="collapsible-trigger"
+    className={cn(className)}
+    {...props}
+  />
+));
+CollapsibleTrigger.displayName = "CollapsibleTrigger";
 
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
+const CollapsibleContent = React.forwardRef<
+  React.ComponentRef<typeof CollapsiblePrimitive.CollapsibleContent>,
+  React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.CollapsibleContent>
+>(({ className, ...props }, ref) => (
+  <CollapsiblePrimitive.CollapsibleContent
+    ref={ref}
+    data-slot="collapsible-content"
+    className={cn(className)}
+    {...props}
+  />
+));
+CollapsibleContent.displayName = "CollapsibleContent";
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent };
